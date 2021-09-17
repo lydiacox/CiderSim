5abf2ed91c01296fb69090276bb99b8abd0a1e50
lydiacox <coxlydia01@gmail.com>
2021-09-17 12:36:36 +1000

    add comments

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from classes import Apple, Pear, AppleTree, PearTree, Cider
+# importing all our handy classes
+from classes import AppleTree, PearTree, Cider
 
+# create a bunch of trees using a list comprehension
 trees = [AppleTree() for x in range (5)]
 
+# telling all the trees to grow fruit
 for tree in trees:
     tree.blossom()
-    print(tree.fruits)
 
+# collecting a crop by harvesting each tree in turn
 crop = []
-
 for tree in trees:
     crop.extend(tree.harvest())
 
+# making cider from the group
 print(Cider(crop))
